bc738a07f91ac335d9bd573df59ea6a1597a6336
Yassine <yassine.e@flowdesignssolutions.com>
2021-01-19 16:57:04 +0100

    First Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,202 @@
+@extends('layout.app')
+@section('title') {{__('Tealwallet - Expense Manager Application')}} @endsection
+@section('content')
+    <div class="text-center my-12">
+        <h1 class="text-tealwallet font-medium text-4xl my-6">Expense Manager System</h1>
+        <p class="bg-teal-400">
+            <b>Tealwallet</b> is a a mobile and web application for expense managment system 
+        </p>
+    </div>
+
+    <div class="grid grid-cols-4 gap-8">
+        <!-- First Value -->
+        <div class="hover:shadow-xl hover:bg-gray-50 cursor-pointer bg-white shadow-lg py-2 px-4 rounded">
+            <div class="text-center border-b py-8 mb-8">
+                <img class="bg-gray-100 rounded-full h-24 w-24 mx-auto" src="https://www.similarweb.com/images/home/sections/home-solutions-1.svg" alt="">
+                <h1 class="text-tealwallet font-medium text-2xl my-6">Manage Daily Expenses</h1>
+                <p class="bg-teal-400 text-base">
+                    It's better to have an idea about your daily, weekly and monthly expense
+                </p>
+            </div>
+
+            <div class="flex pb-4">
+                <div class="w-16 mt-2">
+                    <div class="mx-auto w-5 h-5 pt-1 bg-tealwallet text-xs text-white rounded-full text-center"><i class="fas fa-check"></i></div>
+                </div>
+                <div class="text-sm">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                </div>
+            </div>
+
+            <div class="flex pb-4">
+                <div class="w-16 mt-2">
+                    <div class="mx-auto w-5 h-5 pt-1 bg-tealwallet text-xs text-white rounded-full text-center"><i class="fas fa-check"></i></div>
+                </div>
+                <div class="text-sm">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                </div>
+            </div>
+            <div class="flex pb-4">
+                <div class="w-16 mt-2">
+                    <div class="mx-auto w-5 h-5 pt-1 bg-tealwallet text-xs text-white rounded-full text-center"><i class="fas fa-check"></i></div>
+                </div>
+                <div class="text-sm">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                </div>
+            </div>
+
+            <div class="flex pb-4">
+                <div class="w-16 mt-2">
+                    <div class="mx-auto w-5 h-5 pt-1 bg-tealwallet text-xs text-white rounded-full text-center"><i class="fas fa-check"></i></div>
+                </div>
+                <div class="text-sm">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                </div>
+            </div>
+        </div>
+
+        <!-- Second Value -->
+        <div class="hover:shadow-xl hover:bg-gray-50 cursor-pointer bg-white shadow-lg py-2 px-4 rounded">
+            <div class="text-center border-b py-8 mb-8">
+                <img class="bg-gray-100 rounded-full h-24 w-24 mx-auto" src="https://www.similarweb.com/images/home/sections/home-solutions-2.svg" alt="">
+                <h1 class="text-tealwallet font-medium text-2xl my-6">Global Access</h1>
+                <p class="bg-teal-400 text-base">
+                    It's better to have an idea about your daily, weekly and monthly expense
+                </p>
+            </div>
+
+            <div class="flex pb-4">
+                <div class="w-16 mt-2">
+                    <div class="mx-auto w-5 h-5 pt-1 bg-tealwallet text-xs text-white rounded-full text-center"><i class="fas fa-check"></i></div>
+                </div>
+                <div class="text-sm">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                </div>
+            </div>
+
+            <div class="flex pb-4">
+                <div class="w-16 mt-2">
+                    <div class="mx-auto w-5 h-5 pt-1 bg-tealwallet text-xs text-white rounded-full text-center"><i class="fas fa-check"></i></div>
+                </div>
+                <div class="text-sm">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                </div>
+            </div>
+            <div class="flex pb-4">
+                <div class="w-16 mt-2">
+                    <div class="mx-auto w-5 h-5 pt-1 bg-tealwallet text-xs text-white rounded-full text-center"><i class="fas fa-check"></i></div>
+                </div>
+                <div class="text-sm">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                </div>
+            </div>
+
+            <div class="flex pb-4">
+                <div class="w-16 mt-2">
+                    <div class="mx-auto w-5 h-5 pt-1 bg-tealwallet text-xs text-white rounded-full text-center"><i class="fas fa-check"></i></div>
+                </div>
+                <div class="text-sm">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                </div>
+            </div>
+        </div>
+
+        <!-- Third Value -->
+        <div class="hover:shadow-xl hover:bg-gray-50 cursor-pointer bg-white shadow-lg py-2 px-4 rounded">
+            <div class="text-center border-b py-8 mb-8">
+                <img class="bg-gray-100 rounded-full h-24 w-24 mx-auto " src="https://icon-library.com/images/statistics-icon-png/statistics-icon-png-18.jpg" alt="">
+                <h1 class="text-tealwallet font-medium text-2xl my-6">Detailed Reports</h1>
+                <p class="bg-teal-400 text-base">
+                    It's better to have an idea about your daily, weekly and monthly expense
+                </p>
+            </div>
+
+            <div class="flex pb-4">
+                <div class="w-16 mt-2">
+                    <div class="mx-auto w-5 h-5 pt-1 bg-tealwallet text-xs text-white rounded-full text-center"><i class="fas fa-check"></i></div>
+                </div>
+                <div class="text-sm">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                </div>
+            </div>
+
+            <div class="flex pb-4">
+                <div class="w-16 mt-2">
+                    <div class="mx-auto w-5 h-5 pt-1 bg-tealwallet text-xs text-white rounded-full text-center"><i class="fas fa-check"></i></div>
+                </div>
+                <div class="text-sm">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                </div>
+            </div>
+            <div class="flex pb-4">
+                <div class="w-16 mt-2">
+                    <div class="mx-auto w-5 h-5 pt-1 bg-tealwallet text-xs text-white rounded-full text-center"><i class="fas fa-check"></i></div>
+                </div>
+                <div class="text-sm">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                </div>
+            </div>
+
+            <div class="flex pb-4">
+                <div class="w-16 mt-2">
+                    <div class="mx-auto w-5 h-5 pt-1 bg-tealwallet text-xs text-white rounded-full text-center"><i class="fas fa-check"></i></div>
+                </div>
+                <div class="text-sm">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                </div>
+            </div>
+        </div>
+
+        <!-- Fourth Value -->
+        <div class="hover:shadow-xl hover:bg-gray-50 cursor-pointer bg-white shadow-lg py-2 px-4 rounded">
+            <div class="text-center border-b py-8 mb-8">
+                <img class="bg-gray-100 rounded-full h-24 w-24 mx-auto" src="https://www.similarweb.com/images/home/sections/home-solutions-4.svg" alt="">
+                <h1 class="text-tealwallet font-medium text-2xl my-6">Saving Money</h1>
+                <p class="bg-teal-400 text-base">
+                    It's better to have an idea about your daily, weekly and monthly expense
+                </p>
+            </div>
+
+            <div class="flex pb-4">
+                <div class="w-16 mt-2">
+                    <div class="mx-auto w-5 h-5 pt-1 bg-tealwallet text-xs text-white rounded-full text-center"><i class="fas fa-check"></i></div>
+                </div>
+                <div class="text-sm">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                </div>
+            </div>
+
+            <div class="flex pb-4">
+                <div class="w-16 mt-2">
+                    <div class="mx-auto w-5 h-5 pt-1 bg-tealwallet text-xs text-white rounded-full text-center"><i class="fas fa-check"></i></div>
+                </div>
+                <div class="text-sm">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                </div>
+            </div>
+            <div class="flex pb-4">
+                <div class="w-16 mt-2">
+                    <div class="mx-auto w-5 h-5 pt-1 bg-tealwallet text-xs text-white rounded-full text-center"><i class="fas fa-check"></i></div>
+                </div>
+                <div class="text-sm">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                </div>
+            </div>
+
+            <div class="flex pb-4">
+                <div class="w-16 mt-2">
+                    <div class="mx-auto w-5 h-5 pt-1 bg-tealwallet text-xs text-white rounded-full text-center"><i class="fas fa-check"></i></div>
+                </div>
+                <div class="text-sm">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="text-center text-sm text-gray-400 py-10 mt-7">
+        Tealwallet - {{ date('Y') }} All Rights Reserved
+    </div>
+
+
+@endsection
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {    
+      colors:{
+        'tealwallet': '#4fd1c5'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
